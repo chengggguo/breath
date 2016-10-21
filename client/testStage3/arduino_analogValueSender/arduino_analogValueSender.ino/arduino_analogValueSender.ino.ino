5ca523e60ff961e2e6fef6896e89e67a8578cdfd
chengggguo <chengggguo@gmail.com>
2016-10-21 20:52:34 +1000

    testing  state machine on arduino

--- client/testStage3/arduino_analogValueSender/arduino_analogValueSender.ino/arduino_analogValueSender.ino.ino
+++ client/testStage3/arduino_analogValueSender/arduino_analogValueSender.ino/arduino_analogValueSender.ino.ino
@@ -1,8 +1,15 @@
-int sensor = A0;  // Switch connected to pin 4
+int sensor = A0;
+float sensorCSA = 0.785; // the cross-sectional area of air pressure sensor
+
 int mValue; // mapped value
-boolean check = true;
-boolean case2;
-boolean i;
+boolean check = true; // state check(blowing, inhaling, stand by)
+boolean countingBlow = false;
+unsigned long timeStart;
+unsigned long timeEnd;
+unsigned long interval;
+int maxSpeed = 0;
+int capacity;
+
 
 
 void setup() {
@@ -11,27 +18,49 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("raw " + analogRead(sensor));
   if (check = true) {
     if (analogRead(sensor) >= 110) {
-      Serial.write(0);
-      delay(100);
+      Serial.println("Blowing");
+      //      Serial.write(0);  // blowing
+      countingBlow = true;
+      timeStart = millis();
       check = false;
+      delay(200);
     } else if (analogRead(sensor)<110 and analogRead(sensor) >= 100) {
-      Serial.write(1);
+      Serial.println("stand by");
+      //      Serial.write(1); // stand by
       delay(100);
     } else {
-      Serial.write(2);
-      delay(100);
+      Serial.println("inhaling");
+      //      Serial.write(2);
+      delay(100);  // inhaling
     }
   }
-  
-  if (analogRead(sensor) < 110) {
-    check = true;
-  } else {
-    mValue = int(map(analogRead(sensor), 100, 900, 10, 255));
-    Serial.write(mValue);
-    delay(100);
+  if (countingBlow == true) {
+    if (analogRead(sensor) < 110) {
+
+      interval = timeEnd - timeStart;
+      capacity = interval * maxSpeed * sensorCSA;
+      Serial.println("capacity " + capacity);
+      delay(1000);
+
+      check = true;
+      //    countingBlow = false;
+
+
+    } else {
+      timeEnd = millis();
+      if (analogRead(sensor) > maxSpeed) {
+        maxSpeed = analogRead(sensor);
+
+        //    mValue = int(map(analogRead(sensor), 100, 900, 10, 255));
+        //    Serial.write(mValue);
+        //    delay(100);
+      }
+    }
   }
+  delay(500);
 }
 
 

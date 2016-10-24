cad9216ecab33ced237b9af9d2dd0239b85dab0f
chengggguo <chengggguo@gmail.com>
2016-10-24 19:47:11 +1000

    end of the day

--- client/testStage3/arduino_analogValueSender/arduino_analogValueSender.ino/arduino_analogValueSender.ino.ino
+++ client/testStage3/arduino_analogValueSender/arduino_analogValueSender.ino/arduino_analogValueSender.ino.ino
@@ -1,9 +1,10 @@
 int sensor = A0;
 float sensorCSA = 0.785; // the cross-sectional area of air pressure sensor
 
-int mValue; // mapped value
-boolean check = true; // state check(blowing, inhaling, stand by)
-boolean countingBlow = false;
+boolean stateCheck = true; // state check(blowing, inhaling, stand by)
+boolean Blowing = false;
+boolean inhaling = false;
+
 unsigned long timeStart;
 unsigned long timeEnd;
 int interval = 0;
@@ -13,28 +14,29 @@ int capacity = 0;
 
 
 void setup() {
-  pinMode(sensor, INPUT);             // Set pin 0 as an input
-  Serial.begin(9600);                    // Start serial communication at 9600 bps
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (check = true) {
-    stateMachine(analogRead(sensor));
+  if (stateCheck = true) {
+    stateMachine(analogRead(sensor)); // Three states : blowing, inhaling, stand by
 
   }
-  if (countingBlow == true) {
-    if (analogRead(sensor) < 110) {
+
+  if (Blowing == true) {
+    if (analogRead(sensor) < 111) {
 
       interval = timeEnd - timeStart;
-      capacity = int(interval * maxSpeed * sensorCSA);
-      Serial.println("time " + interval);
-      Serial.print("capacity ");
-      Serial.println(capacity);
-      //      Serial.write(capacity);
+      capacity = int((interval * maxSpeed * sensorCSA) / 10);
+      //      Serial.println("time " + interval);
+      //      Serial.print("capacity ");
+      //      Serial.println(capacity);
+      Serial.write(capacity);
       delay(1000);
 
-      check = true;
-      countingBlow = false;
+      stateCheck = true;
+      Blowing = false;
 
 
     } else {
@@ -43,32 +45,39 @@ void loop() {
         maxSpeed = analogRead(sensor);
       }
     }
+  } else if (inhaling == true) {
+//    Serial.println(capacity + "ms");
+    delay(capacity * 100);
+    interval = 0;
+    stateCheck = true;
+    inhaling = false;
+
   }
   delay(500);
 }
 
 void stateMachine(int i) {
   if (i >= 115) {
-    Serial.println("Blowing");
-    Serial.println(i);
-    //      Serial.write(0);
-    countingBlow = true;
+    //    Serial.println("Blowing");
+    //    Serial.println(i);
+    Serial.write(0);
+    Blowing = true;
     timeStart = millis();
-    check = false;
-    delay(200);
+    stateCheck = false;
+    delay(100);
   } else if (i < 99) {
-    Serial.println("inhaling");
-    Serial.println(i);
-    //     Serial.write(1);
+    //    Serial.println("inhaling");
+    //    Serial.println(i);
+    inhaling = true;
+    stateCheck = false;
+    Serial.write(1);
     delay(100);
   } else {
-    Serial.println("stand by");
-    Serial.println(i);
-    //      Serial.write(2);
+    //    Serial.println("stand by");
+    //    Serial.println(i);
+    Serial.write(2);
     delay(100);
   }
-
-
 }
 
 
